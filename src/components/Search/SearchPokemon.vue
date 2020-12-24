fadfb2b2b2d1a777f99f5d9b0c79396a4e01f4d1
Danielecki <--global>
2020-12-24 14:35:35 +0100

    Add: logic for SearchPokemon

--- src/components/Search/SearchPokemon.vue
+++ src/components/Search/SearchPokemon.vue
@@ -1,10 +1,30 @@
 <template>
-  <div id="search-pokemon"></div>
+  <div id="search-pokemon">
+    <b-col>
+      <h3>SEARCH POKEMON</h3>
+      <input v-model="query" />
+      <button @click="search">Search</button>
+      <button @click="resetForm">Reset</button>
+    </b-col>
+  </div>
 </template>
 
 <script>
 export default {
   name: "SearchPokemon",
+  data() {
+    return {
+      query: "",
+    };
+  },
+  methods: {
+    search() {
+      alert(`hello, your query: ${this.query}`);
+    },
+    resetForm() {
+      this.query = "";
+    },
+  },
 };
 </script>
 
